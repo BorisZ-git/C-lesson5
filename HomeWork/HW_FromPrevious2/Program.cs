ebc8894b9050a646ab8118910a376eb3616ede27
BorisZ-git <ribos@bk.ru>
2017-03-11 23:06:40 +0300

    start Homework 2 from Previously lesson \\ wrote method create

--- HomeWork/HW_FromPrevious2/Program.cs
+++ HomeWork/HW_FromPrevious2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 /* Boris Z
  * ***Написать игру “Верю. Не верю”. В файле хранятся некоторые данные и правда это или нет.
 Например: “Шариковую ручку изобрели в древнем Египте”, “Да”.
@@ -16,6 +17,39 @@ namespace HW_FromPrevious2
     {
         static void Main(string[] args)
         {
+            Create();
+        }
+        static void Create()
+        {
+            #region variables
+            string[] str = new string[10];
+            str[0] = "В Древнем Риме альбомом называли доску, покрытую белым гипсом|Да";
+            str[1] = "На луну воют только волки-одиночки|Нет";
+            str[2] = "Бамбук самая высокая трава в мире.|Да";
+            str[3] = "Авторучка была изобретена ещё в Древнем Египте?|Да";
+            str[4] = "Совы не могут вращать глазами?|Да";
+            str[5] = "Дети могут слышать более высокие звуки, чем взрослые?|Да";
+            str[6] = "Лось является разновидностью оленя?|Да";
+            str[7] = "Мойву эскимосы сушат и едят вместо хлеба?|Да";
+            str[8] = "Радугу можно увидеть и в полночь?|Да";
+            str[9] = "Утром вы выше ростом, чем вечером?|Да";
+
+
+            #endregion
+            StreamWriter stw = null;
+            try
+            {
+                stw = new StreamWriter("data.txt");
+                //stw.WriteLine(str[0]);
+                for (int i = 0; i < str.Length; i++)
+                    stw.WriteLine(str[i]);
+                stw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
         }
     }
 }
