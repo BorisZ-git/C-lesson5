bcff023cbfc245d0140be93c1c4e64baa926e3ab
BorisZ-git <ribos@bk.ru>
2017-03-14 11:48:02 +0300

    using regular\\ wrote search for first char num and search char in string

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -19,15 +19,36 @@ namespace HomeWork
         static void Main()
         {
             // 1.1
-            string Login = Console.ReadLine();            char[] check = Login.ToCharArray();
+            Console.WriteLine("Введите логин: ");
+            string Login = Console.ReadLine();
+            char[] check = Login.ToCharArray();
             if (char.IsDigit(check[0]))
-                Console.WriteLine("Первым символом пароля не может быть цифра!");            if (check.Length<2||check.Length>10)                Console.WriteLine("Логин строго от 2-х до 10-ти символов ");            for (int i = 0; i < check.Length; i++)
-                if (!char.IsLetterOrDigit(check[i]))                    Console.WriteLine("Логин может содержать только буквы или цифры");            Console.WriteLine("Все верно");
+                Console.WriteLine("Первым символом пароля не может быть цифра!");
+            if (check.Length<2||check.Length>10)
+                Console.WriteLine("Логин строго от 2-х до 10-ти символов ");
+            for (int i = 0; i < check.Length; i++)
+                if (!char.IsLetterOrDigit(check[i]))
+                    Console.WriteLine("Логин может содержать только буквы или цифры");
+            //Console.WriteLine("Все верно");
+
             //1.2
-            string LoginReg = Console.ReadLine();            
-            Regex reg = new Regex(@"\w");                Console.WriteLine(reg.IsMatch());                            
+            Console.WriteLine("Введите логин: ");
+            string LoginReg = Console.ReadLine();
+            //Regex reg = new Regex(@"\W|^[0-9]");
+            Regex reg = new Regex(@"\W");
+            Regex regNum = new Regex(@"^[0-9]");
+            if (reg.IsMatch(LoginReg))
+                Console.WriteLine("Логин может содержать только буквы или цифры");
+            if (regNum.IsMatch(LoginReg))
+                Console.WriteLine("Первым символом пароля не может быть цифра!");
+
+
+
+
+
             Console.ReadLine();
         }
-
+
+
     }
 }
