c626925f62d868d3f7b88ee9a2c772fee13b7ca0
BorisZ-git <ribos@bk.ru>
2017-03-14 10:32:37 +0300

    start 1.2

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 /* Boris Z
 1 Создать программу, которая будет проверять корректность ввода логина. Корректным логином
 будет строка от 2-х до 10-ти символов, содержащая только буквы или цифры, и при этом цифра
@@ -21,7 +22,10 @@ namespace HomeWork
             string Login = Console.ReadLine();            char[] check = Login.ToCharArray();
             if (char.IsDigit(check[0]))
                 Console.WriteLine("Первым символом пароля не может быть цифра!");            if (check.Length<2||check.Length>10)                Console.WriteLine("Логин строго от 2-х до 10-ти символов ");            for (int i = 0; i < check.Length; i++)
-                if (!char.IsLetterOrDigit(check[i]))                    Console.WriteLine("Логин может содержать только буквы или цифры");            Console.WriteLine("Все верно");
+                if (!char.IsLetterOrDigit(check[i]))                    Console.WriteLine("Логин может содержать только буквы или цифры");            Console.WriteLine("Все верно");
+            //1.2
+            string LoginReg = Console.ReadLine();            
+            Regex reg = new Regex(@"\w");                Console.WriteLine(reg.IsMatch());                            
             Console.ReadLine();
         }
 
